f09ed33b8eeb0c0f91c039d0c4a2972b2dfd3494
tvdien <tvdien.dn@gmail.com>
2020-02-11 23:11:53 +0900

    add smart-home function

---
+++ scripts/smart-home.js
@@ -0,0 +1,26 @@
+module.exports = function(robot) {
+  function runCommandAndResponse(cmd, msg) {
+    let exec = require('child_process').exec;
+    exec(cmd, function(error, stdout, stderr) {
+      if (error) {
+        msg.send(error);
+        msg.send(stderr);
+      } else {
+        msg.send(stdout);
+      }
+    });
+  }
+
+  robot.hear(/^Smart bot turn (on|off) air conditioner/i, function(msg) {
+    let airCmdText = msg.match[1];
+    let cmd = `cd external-scripts && python3 irrp.py -p -g17 -f codes air:${airCmdText}`;
+    if (!process.env.SMART_HOME) {
+      msg.send('System environment is not defined');
+      msg.send(cmd);
+      return;
+    }
+
+    msg.send(`Sending: Turn ${airCmdText} air conditioner`);
+    runCommandAndResponse(cmd, msg);
+  });
+};
